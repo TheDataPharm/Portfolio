a552aeec42066e23d44005d042369801bae6a838
Oluwatobiloba Richard Omoyele <100296590+TheDataPharm@users.noreply.github.com>
2024-11-21 20:54:47 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const textElement = document.querySelector(".text-animation");
+  const text = textElement.textContent;
+  textElement.innerHTML = '';
+
+  text.split('').forEach((char, index) => {
+    const span = document.createElement('span');
+    span.textContent = char;
+    span.style.setProperty('--char-index', index);
+    textElement.appendChild(span);
+  });
+});
 
